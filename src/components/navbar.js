7f95d7573172e20afed3b712f7849ff93c75869c
Nicholas Norsek <143731641+nnorsek@users.noreply.github.com>
2025-10-06 21:13:33 -0400

    initial commit

---
+++ src/components/navbar.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./componentStyles/Navbar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCar,
+  faPhone,
+  faAngleDown,
+  faCircleUser,
+} from "@fortawesome/free-solid-svg-icons";
+const Navbar = () => {
+  return (
+    <nav className="navbar-container">
+      <div className="logo">
+        <FontAwesomeIcon icon={faCar} />
+        <p>Auto Sales</p>
+      </div>
+      <div className="phone-number">
+        {" "}
+        <FontAwesomeIcon icon={faPhone} />
+        <p>+21 435-543-1000</p>
+      </div>
+      <ul className="horizontal-list">
+        <li>
+          Home <FontAwesomeIcon icon={faAngleDown} />
+        </li>
+        <li>
+          Listing <FontAwesomeIcon icon={faAngleDown} />{" "}
+        </li>
+        <li>
+          Blog <FontAwesomeIcon icon={faAngleDown} />
+        </li>
+        <li>
+          Pages <FontAwesomeIcon icon={faAngleDown} />
+        </li>
+        <li>About</li>
+        <li>Contact</li>
+        <li>
+          <FontAwesomeIcon icon={faCircleUser} /> Sign in
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
